cddb31635f555a81bc16993b109953a5da98534e
TrongAnh16 <anhtrduong@gmail.com>
2022-11-01 15:15:30 +0700

    .

---
+++ README.md
@@ -0,0 +1,13 @@
+## Paint App
+1. Shapes: cho phép người dùng chọn vẽ hình theo các mẫu đã cho sẵn.
+2. Fill color: cho phép bôi màu cho khối hình.
+3. Options: chọn kiểu bút vẽ "Brush" hoặc cục tẩy "Eraser". Với tùy chọn độ lớn của nét vẽ hay cục tẩy trên thanh range (min = 1, max = 100).
+4. Colors: chọn màu để thao tác vẽ. Ở ô màu thứ 5 từ trái sang cho phép người dùng click vào sẽ hiện ra bảng màu với nhiều tùy chọn hơn.
+5. Nút Clear All: cho phép người dùng click vào sẽ xóa tất cả.
+# Kiến thức áp dụng
+1. HTML
+2. CSS
+3. JavaScript
+# Thành quả
+1. Link git: https://github.com/TrongAnh16/PaintApp
+2. Link product: https://tronganh16.github.io/PaintApp/
--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const canvas = document.querySelector("canvas"),
-toolBtns = document.querySelectorAll(".tool"),
-fillColor = document.querySelector("#fill-color"),
-sizeSlider = document.querySelector("#size-slider"),
-colorBtns = document.querySelectorAll(".colors .option"),
-colorPicker = document.querySelector("#color-picker"),
-clearCanvas = document.querySelector(".clear-canvas"),
+const canvas = document.querySelector("canvas");
+toolBtns = document.querySelectorAll(".tool");
+fillColor = document.querySelector("#fill-color");
+sizeSlider = document.querySelector("#size-slider");
+colorBtns = document.querySelectorAll(".colors .option");
+colorPicker = document.querySelector("#color-picker");
+clearCanvas = document.querySelector(".clear-canvas");
 ctx = canvas.getContext("2d");
 
-let prevMouseX, prevMouseY, snapshot,
-isDrawing = false,
-selectedTool = "brush",
-brushWidth = 5,
+let prevMouseX, prevMouseY, snapshot;
+isDrawing = false;
+selectedTool = "brush";
+brushWidth = 5;
 selectedColor = "#000";
 
 const setCanvasBackground = () => {
@@ -54,6 +54,7 @@ const startDraw = (e) => {
     prevMouseY = e.offsetY;
     ctx.beginPath();
     ctx.lineWidth = brushWidth;
+    ctx.lineCap = "round";
     ctx.strokeStyle = selectedColor;
     ctx.fillStyle = selectedColor;
     snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
@@ -62,7 +63,6 @@ const startDraw = (e) => {
 const drawing = (e) => {
     if(!isDrawing) return;
     ctx.putImageData(snapshot, 0, 0); 
-
     if(selectedTool === "brush" || selectedTool === "eraser") {
         ctx.strokeStyle = selectedTool === "eraser" ? "#fff" : selectedColor;
         ctx.lineTo(e.offsetX, e.offsetY);
